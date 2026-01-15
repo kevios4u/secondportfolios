38a53c642567f5fb35306dbf3501c120ec51766e
kevios4u <kevios4u@yahoo.com>
2026-01-15 11:50:57 +0100

    Second Portfolio

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,5 +1,13 @@
+
 const menuToggle = document.getElementById("menuToggle");
 const navLink = document.getElementById("navlink");
 menuToggle.addEventListener("click", () => {
   navLink.classList.toggle("active");
 });
+/* 
+document.querySelectorAll(".nav-link a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLink.classList.remove("active");
+  });
+});
+*/
